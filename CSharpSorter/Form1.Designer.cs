315dd6c39e2a4fa36926fd07d12fabefa2cf9ff7
ArielCann <20acann@gmail.com>
2024-11-01 16:07:41 -0400

    added chart

--- CSharpSorter/Form1.Designer.cs
+++ CSharpSorter/Form1.Designer.cs
@@ -28,12 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            menuStrip1 = new MenuStrip();
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            toolStripMenuItem2 = new ToolStripMenuItem();
+            toolStripMenuItem3 = new ToolStripMenuItem();
+            toolStripMenuItem4 = new ToolStripMenuItem();
+            SortChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)SortChart).BeginInit();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(24, 24);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { toolStripMenuItem1, toolStripMenuItem2, toolStripMenuItem3, toolStripMenuItem4 });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 33);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(116, 29);
+            toolStripMenuItem1.Text = "BubbleSort";
+            toolStripMenuItem1.Click += toolStripMenuItem1_Click;
+            // 
+            // toolStripMenuItem2
+            // 
+            toolStripMenuItem2.Name = "toolStripMenuItem2";
+            toolStripMenuItem2.Size = new Size(98, 29);
+            toolStripMenuItem2.Text = "ShellSort";
+            toolStripMenuItem2.Click += toolStripMenuItem2_Click;
+            // 
+            // toolStripMenuItem3
+            // 
+            toolStripMenuItem3.Name = "toolStripMenuItem3";
+            toolStripMenuItem3.Size = new Size(130, 29);
+            toolStripMenuItem3.Text = "InsertionSort";
+            toolStripMenuItem3.Click += toolStripMenuItem3_Click;
+            // 
+            // toolStripMenuItem4
+            // 
+            toolStripMenuItem4.Name = "toolStripMenuItem4";
+            toolStripMenuItem4.Size = new Size(103, 29);
+            toolStripMenuItem4.Text = "HeapSort";
+            toolStripMenuItem4.Click += toolStripMenuItem4_Click;
+            // 
+            // SortChart
+            // 
+            chartArea2.Name = "ChartArea1";
+            SortChart.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            SortChart.Legends.Add(legend2);
+            SortChart.Location = new Point(227, 36);
+            SortChart.Name = "SortChart";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Array";
+            series2.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            SortChart.Series.Add(series2);
+            SortChart.Size = new Size(505, 353);
+            SortChart.TabIndex = 1;
+            SortChart.Text = "chart1";
+            SortChart.Click += SortChart_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(SortChart);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            Click += Form1_Click;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)SortChart).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem toolStripMenuItem2;
+        private ToolStripMenuItem toolStripMenuItem3;
+        private ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart SortChart;
     }
 }
--- CSharpSorter/Form1.cs
+++ CSharpSorter/Form1.cs
@@ -2,9 +2,50 @@ namespace CSharpSorter
 {
     public partial class Form1 : Form
     {
+        private int[] array = [21, 85, 4, 79, 96, 69, 31, 45, 49, 33, 38, 23, 68, 49, 80, 34, 41,
+ 19, 92, 3, 74, 43, 43, 35, 7, 48, 29, 38, 50, 83];
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SortChart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < array.Length; i++) {
+                SortChart.Series["Array"].Points.AddXY(i, array[i]);
+                SortChart.Series["Array"].Points.ElementAt(i).SetValueY(i);
+            }
         }
     }
 }
